13c791454f63fd5a11975200c831ff2872361fed
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2015-12-03 16:48:59 -0500

    finished code

--- sketch_nov21a.ino
+++ sketch_nov21a.ino
@@ -48,6 +48,11 @@ void setup()
   // MAKE TRUE IF DEBUGGING
   ble.verbose(true);
 
+  Serial.println(F("Setting device name to 'SmartBand B': "));
+  if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=SmartBand B")) ) {
+    error(F("Could not set device name?"));
+  }
+
   Serial.println(F("Adding the Handshake Service definition "));
   int success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID128=00-77-13-12-11-00-00-00-00-00-AB-BA-0F-A1-AF-E1"), &hsServiceId);
   if (!success) {
@@ -55,7 +60,7 @@ void setup()
   }
 
   Serial.println(F("Adding the Handshake Data characteristic "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID128=00-68-42-01-14-88-59-77-42-42-AB-BA-0F-A1-AF-E1,PROPERTIES=0x12,MIN_LEN=1,VALUE=0"), &hsDataCharId);
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=0x4201,PROPERTIES=0x12,MIN_LEN=1,VALUE=0"), &hsDataCharId);
   if (!success) {
     error(F("Could not add Handshake characteristic"));
   }
@@ -69,12 +74,6 @@ void setup()
 
 void loop()
 {
-//  FOR DEBUGGING ONLY
-//  Serial.print(F("AT > "));
-//  char command[BUFSIZE+1];
-//  getUserInput(command, BUFSIZE);
-//  ble.println(command);
-//  ble.waitForOK();
   
   // Get raw accelerometer data for each axis
   int rawX = analogRead(A3);
@@ -89,13 +88,11 @@ void loop()
   }
 
   if(shakes > 6){  
-//    ble.print( F("AT+GATTCHAR=") );
-//    ble.print( hsDataCharId );
-//    ble.print( ",1" );
-//
-//    if (!ble.waitForOK()){
-//      Serial.println("SEND FAIL");
-//    }
+    ble.print( F("AT+GATTCHAR=") );
+    ble.print( hsDataCharId );
+    ble.println( ",1" );
+    ble.waitForOK();
+    Serial.println("HANDSHAKE DETECTED!");
     shakes = 0;
   }
 
@@ -114,19 +111,4 @@ bool within(float x, float t){
 bool withinI(float x, float t){
   return ((x > t) && (675-x > t));
 }
-
-//void getUserInput(char buffer[], uint8_t maxSize)
-//{
-//  memset(buffer, 0, maxSize);
-//  while( Serial.peek() < 0 ) {}
-//  delay(2);
-//
-//  uint8_t count=0;
-//
-//  do
-//  {
-//    count += Serial.readBytes(buffer+count, maxSize);
-//    delay(2);
-//  } while( (count < maxSize) && !(Serial.peek() < 0) );
-//}
 
